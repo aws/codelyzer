d58478b7cefdb68c9b5e2c4adab2b0f925a09a7b
Jon Louie <louiejon@amazon.com>
2021-01-12 14:14:34 -0800

    add temp project directory to .gitignore

---
+++ tst/Codelyzer.Analysis.Tests/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Codelyzer.Analysis.Tests
+{
+    internal class Constants
+    {
+        // Do not change these values without updating the corresponding line in .gitignore:  **/Projects/Temp
+        // This is to prevent test projects from being picked up in git after failed unit tests.
+        internal static readonly string[] TempProjectDirectories = { "Projects", "Temp" };
+    }
+}
