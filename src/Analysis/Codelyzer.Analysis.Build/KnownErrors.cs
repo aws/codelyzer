0514ee15ce387dab19c74b453621e7307a24b7b8
Mark Fawaz <markfawa@amazon.com>
2021-01-20 16:30:23 -0800

    Add log check for build errors

---
+++ src/Analysis/Codelyzer.Analysis.Build/KnownErrors.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codelyzer.Analysis.Build
+{
+    public class KnownErrors
+    {
+        public const string MsBuildMissing = "Couldn't find a .NET Framework MSBuild path";
+    }
+}
