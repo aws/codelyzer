0a77679da44bfc62653fb58bfefeed850f832e2c
Jon Louie <louiejon@amazon.com>
2021-06-02 12:12:43 -0700

    add unit test for file reference extraction

---
+++ tst/Codelyzer.Analysis.Tests/ProjectBuildHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using Codelyzer.Analysis.Build;
+using NUnit.Framework;
+
+namespace Codelyzer.Analysis.Tests
+{
+    public class ProjectBuildHandlerTests
+    {
+        [Test]
+        public void ExtractFileReferencesFromProject_Retrieves_Expected_ReferencePaths()
+        {
+            var projectFileContent = @"
+<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+    <TargetFramework>netcoreapp3.1</TargetFramework>
+  </PropertyGroup>
+  <ItemGroup>
+    <FrameworkReference Include=""Microsoft.AspNetCore.App"" />
+  </ItemGroup>
+  <ItemGroup>
+    <PackageReference Include=""MyFirstPackage"" Version=""1.0.0"" />
+    <PackageReference Include=""MySecondPackage"" Version=""2.0.0"" />
+  </ItemGroup>
+  <ItemGroup>
+    <ProjectReference Include=""TheMainProject"" />
+    <ProjectReference Include=""TheDependency"" />
+  </ItemGroup>
+  <ItemGroup Label=""PortingInfo"">
+  <!-- DO NOT REMOVE WHILE PORTING
+    C:\\RandomFile.dll
+    C:\\this\\is\\some\\path\\to\\Some.dll
+  -->
+  </ItemGroup>
+</Project>";
+            using var stringReader = new StringReader(projectFileContent);
+            var projectFileDoc = XDocument.Load(stringReader);
+            var projectBuildHandlerInstance = new ProjectBuildHandler(null);
+            var extractFileReferencesFromProjectMethod =
+                TestUtils.GetPrivateMethod(projectBuildHandlerInstance.GetType(), "ExtractFileReferencesFromProject");
+
+            // Invoke method and read contents of method output
+            var fileReferences = (List<string>)extractFileReferencesFromProjectMethod.Invoke(projectBuildHandlerInstance, new object[] { projectFileDoc });
+            var expectedFileReferences = new List<string>
+            {
+                @"C:\\RandomFile.dll",
+                @"C:\\this\\is\\some\\path\\to\\Some.dll"
+            };
+
+            CollectionAssert.AreEquivalent(expectedFileReferences, fileReferences);
+        }
+    }
+}
---
+++ tst/Codelyzer.Analysis.Tests/TestUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Codelyzer.Analysis.Tests
+{
+    public class TestUtils
+    {
+        public static MethodInfo GetPrivateMethod(Type type, string methodName)
+        {
+            if (string.IsNullOrWhiteSpace(methodName))
+                Assert.Fail("methodName cannot be null or whitespace");
+
+            var method = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (method == null)
+                Assert.Fail("{0} method not found", methodName);
+
+            return method;
+        }
+    }
+}
