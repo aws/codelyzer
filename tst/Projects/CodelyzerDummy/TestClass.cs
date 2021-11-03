dba05fff2d951ba34f0bfff0697e257fc3bbd470
Biroj Nayak <biroj@amazon.com>
2021-11-03 06:32:47 +0000

    #105 and added more test coverage

---
+++ tst/Projects/CodelyzerDummy/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodelyzerDummy
+{
+    class TestClass : ITest
+    {
+        public string SayHello(string hi)
+        {
+            return "Saying hello to " + hi;
+        }
+    }
+}
