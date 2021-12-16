31b54fe43f909b42052a4dd46abe00a7166f6be7
Aniruddha Dave <aniruddhadave@gmail.com>
2021-12-16 06:07:44 +0530

    test: add unit tests for msbuild fix

--- tst/Codelyzer.Analysis.Tests/Constants.cs
+++ tst/Codelyzer.Analysis.Tests/Constants.cs
@@ -5,5 +5,13 @@
         // Do not change these values without updating the corresponding line in .gitignore:  **/Projects/Temp
         // This is to prevent test projects from being picked up in git after failed unit tests.
         internal static readonly string[] TempProjectDirectories = { "Projects", "Temp" };
+
+        internal static string programFiles = "Program Files";
+        internal static string programFilesx86 = "Program Files (x86)";
+        internal static string vs2022MSBuildPath = @"Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe";
+        internal static string vs2019MSBuildPath = @"Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe";
+        internal static string vs2017MSBuildPath = @"Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe";
+        internal static string vs2019BuildToolsMSBuildPath = @"Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe";
+        internal static string MSBuild14Path = @"Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe";
     }
 }
---
+++ tst/Codelyzer.Analysis.Tests/WorkspaceBuilderHelperTests.cs
@@ -0,0 +1,170 @@
+using Codelyzer.Analysis.Common;
+using Codelyzer.Analysis.Model;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+using Codelyzer.Analysis.Build;
+
+namespace Codelyzer.Analysis.Tests
+{
+    [TestFixture]
+    public class WorkspaceBuilderHelperTest: AwsBaseTest
+    {
+        public string tempDir = "";
+        public string testvs2022Path = "";
+        public string testvs2019Path = "";
+        public string testvs2017And19BuildToolsPath = "";
+        public string testvs2022MissingTargetsPath = "";
+        public string testMSBuild14Path = "";
+        public string testMissingTargetsPath = "";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Setup(GetType());
+            tempDir = GetTstPath(Path.Combine(Constants.TempProjectDirectories));
+            SetupTestProject();
+        }
+
+        private void SetupTestProject() 
+        {
+            var tempDirectory = Directory.CreateDirectory(tempDir);
+            var testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "vs2022Test.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testvs2022Path = Path.Combine(tempDirectory.FullName, "vs2022Test");
+
+            
+            testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "vs2019Test.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testvs2019Path = Path.Combine(tempDirectory.FullName, "vs2019Test");
+
+
+            testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "vs2017And19BuildToolsTest.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testvs2017And19BuildToolsPath = Path.Combine(tempDirectory.FullName, "vs2017And19BuildToolsTest");
+
+
+            testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "vs2022MissingTargetsTest.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testvs2022MissingTargetsPath = Path.Combine(tempDirectory.FullName, "vs2022MissingTargetsTest");
+
+
+            testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "MSBuild14Test.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testMSBuild14Path = Path.Combine(tempDirectory.FullName, "MSBuild14Test");
+
+
+            testProjectZipPath = GetTstPath(Path.Combine("TestProjects", "missingTargetsTest.zip"));
+
+            using (ZipArchive archive = ZipFile.Open(
+                testProjectZipPath, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tempDirectory.FullName);
+            }
+            testMissingTargetsPath = Path.Combine(tempDirectory.FullName, "missingTargetsTest");
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            Directory.Delete(testvs2022Path, true);
+            Directory.Delete(testvs2019Path, true);
+            Directory.Delete(testvs2017And19BuildToolsPath, true);
+            Directory.Delete(testvs2022MissingTargetsPath, true);
+            Directory.Delete(testMSBuild14Path, true);
+            Directory.Delete(testMissingTargetsPath, true);
+        }
+
+        [Test]
+        public void TestVS2022()
+        {
+            string actualMsBuildPath = Path.Combine(testvs2022Path, Constants.vs2022MSBuildPath);
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testvs2022Path, Constants.programFiles), programFilesX86Path: Path.Combine(testvs2022Path, Constants.programFilesx86));
+            Console.WriteLine(actualMsBuildPath);
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual(actualMsBuildPath,msbuildpath);
+        }
+
+        [Test]
+        public void TestVS2019()
+        {
+            string actualMsBuildPath = Path.Combine(testvs2019Path, Constants.vs2019MSBuildPath);
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testvs2019Path, Constants.programFiles), programFilesX86Path: Path.Combine(testvs2019Path, Constants.programFilesx86));
+            Console.WriteLine(actualMsBuildPath);
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual(actualMsBuildPath, msbuildpath);
+        }
+
+        [Test]
+        public void TestVS2017And2019BuildTools()
+        {
+            string actualMsBuildPath = Path.Combine(testvs2017And19BuildToolsPath, Constants.vs2019BuildToolsMSBuildPath);
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testvs2017And19BuildToolsPath, Constants.programFiles), programFilesX86Path: Path.Combine(testvs2017And19BuildToolsPath, Constants.programFilesx86));
+            Console.WriteLine(actualMsBuildPath);
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual(actualMsBuildPath, msbuildpath);
+        }
+
+        [Test]
+        public void TestVS2022MissingTargets()
+        {
+            string actualMsBuildPath = Path.Combine(testvs2022MissingTargetsPath, Constants.vs2019MSBuildPath);
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testvs2022MissingTargetsPath, Constants.programFiles), programFilesX86Path: Path.Combine(testvs2022MissingTargetsPath, Constants.programFilesx86));
+            Console.WriteLine(actualMsBuildPath);
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual(actualMsBuildPath, msbuildpath);
+        }
+
+        [Test]
+        public void TestMSBuild14()
+        {
+            string actualMsBuildPath = Path.Combine(testMSBuild14Path, Constants.MSBuild14Path);
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testMSBuild14Path, Constants.programFiles), programFilesX86Path: Path.Combine(testMSBuild14Path, Constants.programFilesx86));
+            Console.WriteLine(actualMsBuildPath);
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual(actualMsBuildPath, msbuildpath);
+        }
+
+        [Test]
+        public void TestMissingTargets()
+        {
+            string msbuildpath = WorkspaceBuilderHelper.GetFrameworkMsBuildExePath(programFilesPath: Path.Combine(testMissingTargetsPath, Constants.programFiles), programFilesX86Path: Path.Combine(testMissingTargetsPath, Constants.programFilesx86));
+            Console.WriteLine(msbuildpath);
+            Assert.AreEqual("", msbuildpath);
+        }
+    }
+
+}
