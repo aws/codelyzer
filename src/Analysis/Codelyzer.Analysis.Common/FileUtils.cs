dffde698eaf30f8018c1d68b76f2f5f4fcc97d32
Beau Gosse <bgosse@amazon.com>
2022-01-06 16:51:32 -0600

    Adding unit test for CopyDirectory

--- src/Analysis/Codelyzer.Analysis.Common/FileUtils.cs
+++ src/Analysis/Codelyzer.Analysis.Common/FileUtils.cs
@@ -34,27 +34,27 @@ namespace Codelyzer.Analysis.Common
             return path;
         }
 
-        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs = true)
+        public static void DirectoryCopy(string sourceDirPath, string destDirPath, bool copySubDirs = true)
         {
             // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo dir = new DirectoryInfo(sourceDirPath);
             if (!dir.Exists)
             {
                 throw new DirectoryNotFoundException(
                     "Source directory does not exist or could not be found: "
-                    + sourceDirName);
+                    + sourceDirPath);
             }
 
             DirectoryInfo[] dirs = dir.GetDirectories();
 
             // If the destination directory doesn't exist, create it.       
-            Directory.CreateDirectory(destDirName);
+            Directory.CreateDirectory(destDirPath);
 
             // Get the files in the directory and copy them to the new location.
             FileInfo[] files = dir.GetFiles();
             foreach (FileInfo file in files)
             {
-                string tempPath = Path.Combine(destDirName, file.Name);
+                string tempPath = Path.Combine(destDirPath, file.Name);
                 file.CopyTo(tempPath, false);
             }
 
@@ -63,7 +63,7 @@ namespace Codelyzer.Analysis.Common
             {
                 foreach (DirectoryInfo subdir in dirs)
                 {
-                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    string tempPath = Path.Combine(destDirPath, subdir.Name);
                     DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
                 }
             }
---
+++ tst/Codelyzer.Analysis.Tests/FileUtilsTests.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using Codelyzer.Analysis.Common;
+using NUnit.Framework;
+
+namespace Codelyzer.Analysis.Tests
+{
+    [TestFixture]
+    public class FileUtilsTests
+    {
+        private const string SourceDirPath = "FileUtilsTests_SourceDir";
+        private const string DestDirPath = "FileUtilsTests_DestDir";
+
+        [SetUp]
+        public void SetUp()
+        {
+            RemoveTestDirs();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RemoveTestDirs();
+        }
+
+        private static void RemoveTestDirs()
+        {
+            if (Directory.Exists(DestDirPath))
+            {
+                Directory.Delete(DestDirPath, true);
+            }
+            if (Directory.Exists(SourceDirPath))
+            {
+                Directory.Delete(SourceDirPath, true);
+            }
+        }
+
+        [Test]
+        public void DirectoryCopy_CreatesNewDirectory_IfDestDoesNotExists()
+        {
+            // Arrange
+            Directory.CreateDirectory(SourceDirPath);
+            if (Directory.Exists(DestDirPath))
+            {
+                Directory.Delete(DestDirPath);
+            }
+
+            // Act
+            FileUtils.DirectoryCopy(SourceDirPath, DestDirPath, false);
+
+            // Assert
+            Directory.Exists(DestDirPath);
+        }
+
+        [Test]
+        public void DirectoryCopy_Throws_IfSourceDoesNotExists()
+        {
+            // Arrange
+            if (Directory.Exists(SourceDirPath))
+            {
+                Directory.Delete(SourceDirPath);
+            }
+
+            // Act & Assert
+            Assert.Throws<DirectoryNotFoundException>(() => FileUtils.DirectoryCopy(SourceDirPath, DestDirPath, false));
+        }
+
+        [Test]
+        public void DirectoryCopy_CopiesContainedFiles()
+        {
+            // Arrange
+            const string testFileName = "testFile.txt";
+            Directory.CreateDirectory(SourceDirPath);
+            var filePath = Path.Combine(SourceDirPath, testFileName);
+            File.Create(filePath).Dispose();
+
+            // Act
+            FileUtils.DirectoryCopy(SourceDirPath, DestDirPath, true);
+
+            // Assert
+            Assert.IsTrue(File.Exists(Path.Combine(DestDirPath, testFileName)));
+        }
+
+        [Test]
+        public void DirectoryCopy_CopiesSubFolders()
+        {
+            // Arrange
+            const string subFolderName = "SubFolder";
+            const string testFileName = "testFile.txt";
+            Directory.CreateDirectory(SourceDirPath);
+            var subDir = Directory.CreateDirectory(Path.Combine(SourceDirPath, subFolderName)).FullName;
+            var filePath = Path.Combine(subDir, testFileName);
+            File.Create(filePath).Dispose();
+
+            // Act
+            FileUtils.DirectoryCopy(SourceDirPath, DestDirPath, true);
+
+            // Assert
+            Assert.IsTrue(File.Exists(Path.Combine(DestDirPath, subFolderName, testFileName)));
+        }
+    }
+}
