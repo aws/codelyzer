4a9b7f2f272101e0ff16ffadc0f99cd978f6a448
xueningl <xueningl@amazon.com>
2022-06-01 10:29:09 -0700

    remove comments

--- src/Analysis/Codelyzer.Analysis/Analyzer/CSharpAnalyzer.cs
+++ src/Analysis/Codelyzer.Analysis/Analyzer/CSharpAnalyzer.cs
@@ -38,17 +38,5 @@ namespace Codelyzer.Analysis.Analyzer
         }
 
     }
-    /*class PlatinumCreditCard : CreditCard
-    {
-        private readonly string _cardType;
-        private int _creditLimit;
-        private int _annualCharge;
-
-        public PlatinumCreditCard(int creditLimit, int annualCharge)
-        {
-            _cardType = "Platinum";
-            _creditLimit = creditLimit;
-            _annualCharge = annualCharge;
-        }
-    }*/
+    
 }
--- src/Analysis/Codelyzer.Analysis/Analyzer/CSharpAnalyzerFactory.cs
+++ src/Analysis/Codelyzer.Analysis/Analyzer/CSharpAnalyzerFactory.cs
@@ -23,20 +23,5 @@ namespace Codelyzer.Analysis.Analyzer
         }
     }
 
-    /*class MoneyBackFactory : CardFactory
-    {
-        private int _creditLimit;
-        private int _annualCharge;
-
-        public MoneyBackFactory(int creditLimit, int annualCharge)
-        {
-            _creditLimit = creditLimit;
-            _annualCharge = annualCharge;
-        }
-
-        public override CreditCard GetCreditCard()
-        {
-            return new MoneyBackCreditCard(_creditLimit, _annualCharge);
-        }
-    }*/
+    
 }
