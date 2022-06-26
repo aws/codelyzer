f51936ceefaa816c2d47b807b8ea464f89cbe55f
Mike Lin <xqlin@amazon.com>
2022-06-26 18:26:00 +0000

    Cache class and interface declarations.

--- src/Analysis/Codelyzer.Analysis.CSharp/Handlers/ClassDeclarationHandler.cs
+++ src/Analysis/Codelyzer.Analysis.CSharp/Handlers/ClassDeclarationHandler.cs
@@ -2,12 +2,15 @@ using Codelyzer.Analysis.Common;
 using Codelyzer.Analysis.Model;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Codelyzer.Analysis.CSharp.Handlers
 {
     public class ClassDeclarationHandler : UstNodeHandler
     {
+        // Key: class full identifier, value: ClassDeclaration.
+        private static Dictionary<string, ClassDeclaration> _classDelcarationCache = new();
         private ClassDeclaration ClassDeclaration { get => (ClassDeclaration)UstNode; }
 
         public ClassDeclarationHandler(CodeContext context,
@@ -15,43 +18,55 @@ namespace Codelyzer.Analysis.CSharp.Handlers
             : base(context, syntaxNode, new ClassDeclaration())
         {
             var classSymbol = SemanticHelper.GetDeclaredSymbol(syntaxNode, SemanticModel, OriginalSemanticModel);
-            Set(ClassDeclaration, classSymbol);
+            if (classSymbol != null)
+            {
+                var fullIdentifier = classSymbol.OriginalDefinition.ToString();
+                if (_classDelcarationCache.ContainsKey(fullIdentifier))
+                {
+                    UstNode = _classDelcarationCache[fullIdentifier];
+                    return;
+                }
+                Set(ClassDeclaration, classSymbol);
+            }
         }
 
         private void Set(ClassDeclaration classDeclaration, INamedTypeSymbol classSymbol)
         {
-            if (classSymbol != null)
+            var syntaxNodes = classSymbol.DeclaringSyntaxReferences;
+            if (syntaxNodes.Length > 0)
             {
-                var syntaxNodes = classSymbol.DeclaringSyntaxReferences;
-                if (syntaxNodes.Length > 0)
-                {
-                    var syntaxNode = (ClassDeclarationSyntax)syntaxNodes[0].GetSyntax();
-                    classDeclaration.Identifier = syntaxNode.Identifier.ToString();
-                    classDeclaration.Modifiers = syntaxNode.Modifiers.ToString();
-                }
-
-                if (classSymbol.BaseType != null)
-                {
-                    classDeclaration.BaseType = classSymbol.BaseType.ToString();
-                    classDeclaration.BaseTypeOriginalDefinition = GetBaseTypOriginalDefinition(classSymbol);
-                    classDeclaration.Reference.Namespace = GetNamespace(classSymbol);
-                    classDeclaration.Reference.Assembly = GetAssembly(classSymbol);
-                    classDeclaration.Reference.Version = GetAssemblyVersion(classSymbol);
-                    classDeclaration.Reference.AssemblySymbol = classSymbol.ContainingAssembly;
-                    classDeclaration.FullIdentifier = classSymbol.OriginalDefinition.ToString();
-                }
-
-                if (classSymbol.Interfaces != null)
-                {
-                    classDeclaration.BaseList = classSymbol.Interfaces.Select(x => x.ToString())?.ToList();
-                }
-                classDeclaration.BaseTypeDeclaration = GetBaseTypeDeclaration(classSymbol.BaseType);
+                var syntaxNode = (ClassDeclarationSyntax)syntaxNodes[0].GetSyntax();
+                classDeclaration.Identifier = syntaxNode.Identifier.ToString();
+                classDeclaration.Modifiers = syntaxNode.Modifiers.ToString();
             }
+
+            if (classSymbol.BaseType != null)
+            {
+                classDeclaration.BaseType = classSymbol.BaseType.ToString();
+                classDeclaration.BaseTypeOriginalDefinition = GetBaseTypOriginalDefinition(classSymbol);
+                classDeclaration.Reference.Namespace = GetNamespace(classSymbol);
+                classDeclaration.Reference.Assembly = GetAssembly(classSymbol);
+                classDeclaration.Reference.Version = GetAssemblyVersion(classSymbol);
+                classDeclaration.Reference.AssemblySymbol = classSymbol.ContainingAssembly;
+                classDeclaration.FullIdentifier = classSymbol.OriginalDefinition.ToString();
+            }
+
+            if (classSymbol.Interfaces != null)
+            {
+                classDeclaration.BaseList = classSymbol.Interfaces.Select(x => x.ToString())?.ToList();
+            }
+            classDeclaration.BaseTypeDeclaration = GetBaseTypeDeclaration(classSymbol.BaseType);
         }
 
         private ClassDeclaration GetBaseTypeDeclaration(INamedTypeSymbol baseTypeSymbol)
         {
             if (baseTypeSymbol == null) return null;
+
+            var fullIdentifier = baseTypeSymbol.OriginalDefinition.ToString();
+            if (_classDelcarationCache.ContainsKey(fullIdentifier))
+            {
+                return _classDelcarationCache[fullIdentifier];
+            }
 
             ClassDeclaration baseDeclaration = new();
             Set(baseDeclaration, baseTypeSymbol);
--- src/Analysis/Codelyzer.Analysis.CSharp/Handlers/InterfaceDeclarationHandler.cs
+++ src/Analysis/Codelyzer.Analysis.CSharp/Handlers/InterfaceDeclarationHandler.cs
@@ -1,13 +1,15 @@
+using System.Collections.Generic;
 using Codelyzer.Analysis.Common;
 using Codelyzer.Analysis.Model;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Codelyzer.Analysis.CSharp.Handlers
 {
     public class InterfaceDeclarationHandler : UstNodeHandler
     {
+        // Key: interface full identifier, value: InterfaceDeclaration.
+        private static Dictionary<string, InterfaceDeclaration> _interfaceDelcarationCache = new();
         private InterfaceDeclaration InterfaceDeclaration { get => (InterfaceDeclaration)UstNode; }
 
         public InterfaceDeclarationHandler(CodeContext context,
@@ -15,32 +17,43 @@ namespace Codelyzer.Analysis.CSharp.Handlers
             : base(context, syntaxNode, new InterfaceDeclaration())
         {
             var interfaceSymbol = SemanticHelper.GetDeclaredSymbol(syntaxNode, SemanticModel, OriginalSemanticModel);
-            Set(InterfaceDeclaration, interfaceSymbol);
+            if (interfaceSymbol != null)
+            {
+                var fullIdentifier = interfaceSymbol.OriginalDefinition.ToString();
+                if (_interfaceDelcarationCache.ContainsKey(fullIdentifier))
+                {
+                    UstNode = _interfaceDelcarationCache[fullIdentifier];
+                    return;
+                }
+                Set(InterfaceDeclaration, interfaceSymbol);
+            }
         }
         private void Set(InterfaceDeclaration interfaceDeclaration, INamedTypeSymbol interfaceSymbol)
         {
-            if (interfaceSymbol != null)
+            var fullIdentifier = interfaceSymbol.OriginalDefinition.ToString();
+            if (_interfaceDelcarationCache.ContainsKey(fullIdentifier))
             {
-                var syntaxNodes = interfaceSymbol.DeclaringSyntaxReferences;
-                if (syntaxNodes.Length > 0)
-                {
-                    var syntaxNode = (ClassDeclarationSyntax)syntaxNodes[0].GetSyntax();
-                    interfaceDeclaration.Identifier = syntaxNode.Identifier.ToString();
-                }
-                
-                interfaceDeclaration.Reference.Namespace = GetNamespace(interfaceSymbol);
-                interfaceDeclaration.Reference.Assembly = GetAssembly(interfaceSymbol);
-                interfaceDeclaration.Reference.Version = GetAssemblyVersion(interfaceSymbol);
-                interfaceDeclaration.Reference.AssemblySymbol = interfaceSymbol.ContainingAssembly;
-                interfaceDeclaration.FullIdentifier = string.Concat(interfaceDeclaration.Reference.Namespace, ".", interfaceDeclaration.Identifier);
 
-                if (interfaceSymbol.Interfaces != null)
+            }
+            var syntaxNodes = interfaceSymbol.DeclaringSyntaxReferences;
+            if (syntaxNodes.Length > 0)
+            {
+                var syntaxNode = (ClassDeclarationSyntax)syntaxNodes[0].GetSyntax();
+                interfaceDeclaration.Identifier = syntaxNode.Identifier.ToString();
+            }
+            
+            interfaceDeclaration.Reference.Namespace = GetNamespace(interfaceSymbol);
+            interfaceDeclaration.Reference.Assembly = GetAssembly(interfaceSymbol);
+            interfaceDeclaration.Reference.Version = GetAssemblyVersion(interfaceSymbol);
+            interfaceDeclaration.Reference.AssemblySymbol = interfaceSymbol.ContainingAssembly;
+            interfaceDeclaration.FullIdentifier = interfaceSymbol.OriginalDefinition.ToString();
+
+            if (interfaceSymbol.Interfaces != null)
+            {
+                interfaceDeclaration.BaseTypeDeclarationList = new();
+                foreach( var ifs in interfaceSymbol.Interfaces)
                 {
-                    interfaceDeclaration.BaseTypeDeclarationList = new();
-                    foreach( var ifs in interfaceSymbol.Interfaces)
-                    {
-                        interfaceDeclaration.BaseTypeDeclarationList.Add(GetBaseTypeDeclaration(ifs));
-                    }
+                    interfaceDeclaration.BaseTypeDeclarationList.Add(GetBaseTypeDeclaration(ifs));
                 }
             }
         }
@@ -48,6 +61,12 @@ namespace Codelyzer.Analysis.CSharp.Handlers
         private InterfaceDeclaration GetBaseTypeDeclaration(INamedTypeSymbol baseTypeSymbol)
         {
             if (baseTypeSymbol == null) return null;
+
+            var fullIdentifier = baseTypeSymbol.OriginalDefinition.ToString();
+            if (_interfaceDelcarationCache.ContainsKey(fullIdentifier))
+            {
+                return _interfaceDelcarationCache[fullIdentifier];
+            }
 
             InterfaceDeclaration baseDeclaration = new();
             Set(baseDeclaration, baseTypeSymbol);
