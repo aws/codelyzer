fe83bb727686090063e98e33084dbbca53ca5dce
Jon Louie <louiejon@amazon.com>
2023-09-17 17:56:13 -0700

    Add tests for TypeParameter handling

--- tst/Codelyzer.Analysis.Tests/TestUtils.cs
+++ tst/Codelyzer.Analysis.Tests/TestUtils.cs
@@ -19,6 +19,19 @@ namespace Codelyzer.Analysis.Tests
             return method;
         }
 
+        public static MethodInfo GetPrivateStaticMethod(Type type, string methodName)
+        {
+            if (string.IsNullOrWhiteSpace(methodName))
+                Assert.Fail("methodName cannot be null or whitespace");
+
+            var method = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (method == null)
+                Assert.Fail("{0} method not found", methodName);
+
+            return method;
+        }
+
         public static PropertyInfo GetPrivateProperty(Type type, string propertyName)
         {
             if (string.IsNullOrWhiteSpace(propertyName))
